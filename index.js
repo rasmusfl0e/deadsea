d0c2a20129e611615580d4a5ced7ea2839b4d0e0
rasmusfl0e <rasmusfl0e@gmail.com>
2016-05-12 22:57:33 +0200

    added wheel support

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ module.exports = function (container) {
 	var scrollStart = null;
 	var scrollNow = 0;
 	var scrollPrev;
-	var prev, next, lastMove, velocity;
-	var height = document.body.offsetHeight;
+	var prev, next, timePrev, velocity;
+	var height = container.offsetHeight;
+	
+	
 
 	function touchstart (event) {
 		if (scrollStart === null) {
 			var touches = event.touches || event.changedTouches;
 			scrollStart = ((touches) ? touches[0] : event).clientY;
-			height = document.body.offsetHeight;
+			scrollNow = scrollStart;
+			timePrev = new Date();
+			height = container.offsetHeight;
 			slides[currentIndex].style.transition = "none";
 			event.preventDefault();
 		}
@@ -28,68 +32,130 @@ module.exports = function (container) {
 
 	function touchmove (event) {
 		if (scrollStart !== null) {
-			var now = new Date();
+			var timeNow = new Date();
 			scrollPrev = scrollNow;
 			var touches = event.touches || event.changedTouches;
 			scrollNow = ((touches) ? touches[0] : event).clientY;
-			velocity = Math.abs(scrollNow - scrollPrev) / (now - lastMove);
-			lastMove = now;
+			velocity = Math.abs(scrollNow - scrollPrev) / (timeNow - timePrev);
+			timePrev = timeNow;
 			var offset = ((scrollNow - scrollStart) / height * 100);
-			if (offset < 0) {
-				if (currentIndex < slides.length - 1) {
-					if (!next) {
-						next = slides[currentIndex + 1];
-						next.style.transition = "none";
-					}
-				}
-			}
-			else {
-				if (currentIndex > 0) {
-					if (!prev) {
-						prev = slides[currentIndex - 1];
-						prev.style.transition = "none";
-					}
-				}
-			}
-			slides[currentIndex].style.transform = "translate3d(0, " + offset + "%, 0)";
-			if (next) {
-				next.style.transform = "translate3d(0, " + (offset + 100) + "%, 0)";
-			}
-			if (prev) {
-				prev.style.transform = "translate3d(0, " + (offset - 100) + "%, 0)";
-			}
+			updateOffset(offset);
 			event.preventDefault();
 		}
 	}
 
 	function touchend (event) {
 		if (scrollStart !== null) {
-			var diffTime = new Date() - lastMove;
+			var diffTime = new Date() - timePrev;
 			var _velocity = (diffTime) ? (velocity / diffTime) : velocity;
 			var fast = (_velocity > 1);
-			slides[currentIndex].style.transition = "";
-			slides[currentIndex].style.transform = "";
-			if (prev) {
-				prev.style.transition = "";
-				prev.style.transform = "";
-				prev = null;
-			}
-			if (next) {
-				next.style.transition = "";
-				next.style.transform = "";
-				next = null;
-			}
-			var scrollEnd = scrollNow - scrollStart;
-			var scrollEndOffset = Math.abs(scrollEnd / height * 100);
-			var mod = (scrollEnd > 0) ? -1 : 1;
-			if ((fast && scrollEndOffset > 10) || scrollEndOffset > 25) {
-				go(currentIndex + mod);
-			}
-			scrollNow = 0;
+			resetOffset();
+			var scrollEndOffset = (scrollNow - scrollStart) / height * 100;
+			snapOffset(scrollEndOffset, fast);
 			scrollStart = null;
-			lastMove = null;
 			event.preventDefault();
 		}
+	}
+	
+	function updateOffset (offset) {
+		if (offset < 0) {
+			if (currentIndex < slides.length - 1) {
+				if (!next) {
+					next = slides[currentIndex + 1];
+					next.style.transition = "none";
+				}
+			}
+		}
+		else {
+			if (currentIndex > 0) {
+				if (!prev) {
+					prev = slides[currentIndex - 1];
+					prev.style.transition = "none";
+				}
+			}
+		}
+		slides[currentIndex].style.transform = "translate3d(0, " + offset + "%, 0)";
+		if (next) {
+			next.style.transform = "translate3d(0, " + (offset + 100) + "%, 0)";
+		}
+		if (prev) {
+			prev.style.transform = "translate3d(0, " + (offset - 100) + "%, 0)";
+		}
+	}
+	
+	function snapOffset (offset, fast) {
+		var direction = (offset > 0) ? -1 : 1;
+		offset = Math.abs(offset);
+		if ((fast && offset > 10) || offset > 25) {
+			go(currentIndex + direction);
+		}
+	}
+	
+	function resetOffset () {
+		slides[currentIndex].style.transition = "";
+		slides[currentIndex].style.transform = "";
+		if (prev) {
+			prev.style.transition = "";
+			prev.style.transform = "";
+			prev = null;
+		}
+		if (next) {
+			next.style.transition = "";
+			next.style.transform = "";
+			next = null;
+		}
+	}
+
+	var timer, interval = 100;
+	var offsetY;
+	var lastY;
+	var velocities = [];
+	var scrolling;
+	var incremental;
+	
+	function timeout () {
+		//scrolling = false;
+		timePrev = null;
+		velocity = 0;
+		timer = clearTimeout(timer);
+		resetOffset();
+		snapOffset(offsetY);
+		window.console && console.log("wheel end", incremental)
+		window.console && console.table(velocities);
+		velocities = [];
+		lastY = 0;
+		incremental = false;
+	}
+
+	function wheel (event) {
+		var timeNow = new Date();
+		var y = event.deltaY;
+		if (timer) {
+			if (velocities.length === 0 && Math.abs(lastY) > 4 && Math.abs(lastY * 10 - y) > 0) {
+				incremental = true;
+			}
+			if (incremental) {
+				var _y = y;
+				y -= (lastY > 0) ? lastY : -lastY;
+				lastY = _y;
+			}
+			else {
+				lastY = y;
+			}
+			offsetY += y;
+			velocity = (y) ? y / (timeNow - timePrev) : 0;
+			velocities.push([y, timeNow - timePrev, velocity]);
+		}
+		else {
+			offsetY = y;
+			height = container.offsetHeight;
+			velocities = [];
+			window.console && console.log("wheel start", y, event.deltaMode);
+		}
+		updateOffset(offsetY / height * 100);
+		timePrev = timeNow;
+		clearTimeout(timer);
+		timer = setTimeout(timeout, interval);
 	}
 
 	function keydown (event) {
@@ -103,6 +169,8 @@ module.exports = function (container) {
 				break;
 		}
 	}
+
+	container.addEventListener("wheel", wheel);
 
 	window.addEventListener("keydown", keydown);
 
